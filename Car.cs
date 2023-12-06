865ea5e59d02d7c204fdce6a8fc13de7bcffea75
SuyogSky <shakyasuyog214@gmail.com>
2023-12-06 09:30:48 +0545

    Exercise 1

---
+++ Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Car
+    {
+        private string brand;
+        private int year;
+        private float price;
+
+        public Car(string brand, int year, float price)
+        {
+            this.brand = brand;
+            this.year = year;
+            this.price = price;
+        }
+
+        public void displayCarInfo()
+        {
+            Console.WriteLine($"Brand: {brand}");
+            Console.WriteLine($"Year: {year}");
+            Console.WriteLine($"Price: {price:C}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car bmw = new Car("BMW", 2023, 30000);
+            bmw.displayCarInfo();
+        }
+    }
+
+
+}
