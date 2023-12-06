09c836188026b4b003a121983190606755a643c4
SuyogSky <shakyasuyog214@gmail.com>
2023-12-06 10:13:16 +0545

    Exercise 2

---
+++ BankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class BankAccount
+    {
+        private String accountNumber;
+        private decimal balance;
+
+        public BankAccount(String accountNumber)
+        {
+            this.accountNumber = accountNumber;
+            this.balance = 0;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            balance -= amount;
+        }
+
+        public void DisplayBalance()
+        {
+            Console.WriteLine($"Your balance is Rs. {balance}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,8 +4,15 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\nExercise 1>");
             Car bmw = new Car("BMW", 2023, 30000);
             bmw.displayCarInfo();
+
+            Console.WriteLine("\nExercise 2>");
+            BankAccount suyog = new BankAccount("22016093");
+            suyog.Deposit(1000);
+            suyog.Withdraw(500);
+            suyog.DisplayBalance();
         }
     }
 
