aa6068e86e3b5c48ca24517927cfdb9ef2115726
SuyogSky <shakyasuyog214@gmail.com>
2023-12-06 10:43:30 +0545

    Exercise 3

--- Program.cs
+++ Program.cs
@@ -13,6 +13,10 @@
             suyog.Deposit(1000);
             suyog.Withdraw(500);
             suyog.DisplayBalance();
+
+            Console.WriteLine("\nExercise 3>");
+            Rectangle r1 = new Rectangle(4.5f, 3.2f);
+            Console.WriteLine($"The area of rectangle is: {r1.CalculateArea()}");
         }
     }
 
---
+++ Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Rectangle
+    {
+        private float length;
+        private float width;
+
+        public Rectangle(float length, float width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public float CalculateArea()
+        {
+            return length * width;
+        }
+    }
+}
